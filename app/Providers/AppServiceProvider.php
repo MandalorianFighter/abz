98178d79ece6d7857679667751693ea51214d7d9
MandalorianFighter <yanush.polishchuk@gmail.com>
2022-12-27 20:45:25 +0200

    add debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        //check that app is local
+        if ($this->app->isLocal()) {
+        //if local register your services you require for development
+            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+        } else {
+        //else register your services you require for production
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,6 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'Image' => Intervention\Image\Facades\Image::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
